290941c99fe1959748bb3ee56b3a4e6287caed6f
邢跃锋 <xyf@nihaosi.com>
2018-04-17 23:31:46 +0800

    feat: 完善cookie的增加 读取 删去 简化版功能

--- cookies.js
+++ cookies.js
@@ -27,22 +27,52 @@
           { path: '/' },
           attributes
         )
+        // 设置cookie有效期
+        const expires = Number(attributes.expires);
+        if (typeof expires === "number") {
+          attributes.expires = new Date(new Date() * 1 + expires * 864e+5);
+        }
+        // 将attributes转成字符串
+        let stringAttributes = '';
+        for(let key in attributes) {
+          stringAttributes += `;${key}=${attributes[key]}`;
+        }
+        value = String(value);
+        key = String(key);
+        return ( document.cookie = `${key}=${value}${stringAttributes}`)
       }
-      // 设置cookie有效期
-      const expires = Number(attributes.expires);
-      if (typeof expires === "number") {
-        attributes.expires = new Date(new Date() * 1 + expires * 864e+5);
+
+      //read
+      const obj = {}
+      const cookies = document.cookie ? document.cookie.split('; ') : [];
+      for(let i = 0; i < cookies.length; i += 1) {
+        // [ "ceshi=hahah" ]
+        const parts = cookies[i].split('=');
+        const cookie = parts.slice(1).join('');
+        if (!this.json && cookie.charAt(0) === '"') {
+					cookie = cookie.slice(1, -1);
+        }
+        const name = parts[0];
+        if(key === name) {
+          obj[key] = cookie;
+          break;
+        }
       }
-      // 将attributes转成字符串
-      let stringAttributes = '';
-      for(let key in attributes) {
-        stringAttributes += `;${key}=${attributes[key]}`;
-      }
-      value = String(value);
-      key = String(key);
-      return ( document.cookie = `${key}=${value}${stringAttributes}`)
+      return key ? obj[key] : obj
+    }
+
+   
+    
+    api.get = function(key) {
+      return api.call(api, key);
     }
     api.set = api;
+    api.rm = function(key, attributes) {
+      api.set(key, '', extend({
+        attributes,
+        expires: -1,
+      }))
+    }
     return api;
   }
   return init();
